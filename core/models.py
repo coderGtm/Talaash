98e61f05f30933e60675625708dc92859e77674b
coderGtm <gautammehta0000@gmail.com>
2023-01-30 23:27:16 +0530

    created models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Keywords(models.Model):
+    keyword = models.CharField(max_length=100)
+    def __str__(self):
+        return self.keyword
+
+class Links(models.Model):
+    link_url = models.CharField(max_length=100)
+    link_ref_num = models.IntegerField()
+    link_keywords = models.ManyToManyField(Keywords)
+    def __str__(self):
+        return self.link
