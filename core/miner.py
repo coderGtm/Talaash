7b1e0ac2be230b3338d9c1db1087f1bc1b3daf5e
coderGtm <gautammehta0000@gmail.com>
2023-02-01 13:28:07 +0530

    data mining operations set for now

--- core/miner.py
+++ core/miner.py
@@ -13,12 +13,13 @@ def scrap(url):
     urls_found_on_this_page = set()
     for link in soup.find_all('a', href = True):
         found_url = str(link.get('href'))
-        if len(found_url) > 1 and found_url[0] == "/":
-            found_url = urljoin(url,found_url)
-            if re.match(url_regex, found_url) is not None:
+        if len(found_url) > 0:
+            if found_url[0] == "/":
+                found_url = urljoin(url,found_url)
+            if found_url != url and re.match(url_regex, found_url) is not None:
                 urls_found_on_this_page.add(found_url)
     
-    print(urls_found_on_this_page)
+    urls_found_on_this_page = list(urls_found_on_this_page)
 
     keywords = []
     if soup.find_all('meta', attrs={'name':'description'}):
@@ -34,16 +35,16 @@ def scrap(url):
     for title in titles:
         keywords.append(str(title.text).strip())
 
-    return keywords, urls_found_on_this_page
+    return (keywords, urls_found_on_this_page)
 
 
 
 def store(url, keywords, urls_found_on_this_page):
+    print(urls_found_on_this_page)
     # saving current url to db if not already saved, and getting its reference, later mapping it with keywords and updating its last_scrapped value
     url_row, created_url_obj = Urls.objects.get_or_create(address = url)
     # save keywords to database model Keywords and relate it with current url by many-to-many relationship
     for keyword in keywords:
-        print(keyword)
         keyword = keyword.strip()
         if keyword:
             keyword_row, created_keyword_obj = Keywords.objects.get_or_create(keyword_string = keyword)
@@ -54,6 +55,10 @@ def store(url, keywords, urls_found_on_this_page):
     # not yet scrapped can be determined by last_scrapped being datetime.datetime.min
     for link in urls_found_on_this_page:
         link_row, created_link_obj = Urls.objects.get_or_create(address = link)
+        print("DEBUG\n\n")
+        print(url_row.last_scrapped)
+        print(datetime.datetime.min)
+        print( url_row.last_scrapped == datetime.datetime.min)
         if not created_link_obj and url_row.last_scrapped == datetime.datetime.min:
             link_row.num_of_refs += 1
             link_row.save()
@@ -77,6 +82,6 @@ def get_url_regex():
 
 if __name__ == "django.core.management.commands.shell":
     url_regex = get_url_regex()
-    url = "https://github.com/coderGtm?tab=repositories"
+    url = "https://coderGtm.github.io/projects"
     keywords, urls_found_on_this_page = scrap(url)
     store(url, keywords, urls_found_on_this_page)
