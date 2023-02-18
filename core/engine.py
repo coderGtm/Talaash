1bee98fe6a65cfbc116fe846943a2740fc03bcdd
coderGtm <gautammehta0000@gmail.com>
2023-02-18 12:27:21 +0530

    added individual word based search

--- core/engine.py
+++ core/engine.py
@@ -7,10 +7,16 @@ def getResults(query):
     for item in Keywords.objects.filter(keyword_string__contains = query):
         for q in Urls.objects.filter(keywords_in_it__exact = item.id):
             matchingUrls.append([q.address, q.num_of_refs, q.page_title, q.page_description, q.icon_link])
+
+    individualWords = query.split(" ")
+    for word in individualWords:
+        for item in Keywords.objects.filter(keyword_string__contains = word):
+            for q in Urls.objects.filter(keywords_in_it__exact = item.id):
+                matchingUrls.append([q.address, q.num_of_refs, q.page_title, q.page_description, q.icon_link])
             
     matchingUrls.sort(key=getNumOfRefs, reverse=True)
 
-    print("------Talaash Results-------")
+    print("------Talaash Results for {0}-------".format(query))
     
     for mu in matchingUrls:
         url = mu[0]
@@ -27,4 +33,4 @@ def getResults(query):
 def getNumOfRefs(elem):
     return elem[1]
 
-getResults("charusat")
+getResults("India")
